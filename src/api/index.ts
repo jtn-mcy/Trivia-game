250e2d49e278ab0ff65f3e503c63960005ed7b2c
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-20 19:58:31 -0700

    add get questions hooks

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,5 +19,26 @@ export const useGetRandomQuestion = () => {
   return questionsJSON[Math.floor(Math.random() * questionsJSON.length)]
 }
 
+export const useGetRandomQuestions: ( numQuestions: number ) => Question[] = (numQuestions) => {
+  let flag = 0;
+  const questions: Question[] = [];
+  while (flag < numQuestions) {
+    const questionToAdd = questionsJSON[Math.floor(Math.random() * questionsJSON.length)]
+    if (!questions.includes(questionToAdd)) {
+      questions.push(questionToAdd)
+      flag++
+    }
+  }
+
+  return questions
+}
+
+export const useGetCurrentQuestion: () => Question = () => {
+  const questions = useAppSelector(state => state.question.questions)
+  const index = useAppSelector(state => state.question.index)
+
+  return questions[index]
+}
+
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
