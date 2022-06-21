419e5f8c74217262709380b7dc0c1970563c9b22
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-21 13:08:37 -0700

    add go to next question

--- src/components/QuestionCard/index.tsx
+++ src/components/QuestionCard/index.tsx
@@ -1,14 +1,49 @@
-import React, { useState } from 'react';
-import { useAppDispatch, useGetCurrentQuestion } from '../../api';
+import React, { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector, useGetCurrentQuestion } from '../../api';
 import styles from './index.module.scss';
 import { increment as incrementScore, decrement as decrementScore } from '../../state/score/scoreSlice';
-import { increment as incrementQuestion } from '../../state/questions/questionSlice'
+import { increment as incrementQuestion, clearQuestions } from '../../state/questions/questionSlice'
+import Button from '../Button';
+import { useNavigate } from 'react-router-dom';
+
+type GoNextType = {
+  isLastQuestion: boolean
+  setAnswer: React.Dispatch<React.SetStateAction<string | undefined>>
+  setIsCorrect: React.Dispatch<React.SetStateAction<boolean | undefined>> 
+}
+
+const GoNext: React.FC<GoNextType> = ( {setIsCorrect, isLastQuestion, setAnswer} ) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const handleGoNext: () => void = () => {
+    if (!isLastQuestion) {
+      setIsCorrect(undefined);
+      setAnswer(undefined);
+      dispatch(incrementQuestion())
+    } else {
+      setAnswer(undefined);
+      dispatch(clearQuestions())
+      navigate('/highscore')
+    }
+  } 
+  return (
+    <Button btnType='NextQuestion' text={!isLastQuestion ? 'Next question}': 'Finish game!'} onClick={handleGoNext} />
+  )
+}
 
 const QuestionCard: React.FC = () => {
   const [answer, setAnswer] = useState<string | undefined>();
   const [isCorrect, setIsCorrect] = useState<boolean | undefined>();
+  const [isLastQuestion, setIsLastQuestion] = useState<boolean>(false);
+
   const dispatch = useAppDispatch();
   const question = useGetCurrentQuestion();
+  const questionState = useAppSelector(state => state.question);
+
+  useEffect(() => {
+    if (questionState.index === questionState.questions.length-1) setIsLastQuestion(true);
+  }, [question])
+
   const handleFormSubmission = (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -19,7 +54,6 @@ const QuestionCard: React.FC = () => {
       dispatch(decrementScore());
       setIsCorrect(false);
     }
-    dispatch(incrementQuestion())
   }
 
   return (
@@ -43,9 +77,15 @@ const QuestionCard: React.FC = () => {
         </form>
         {isCorrect !== undefined ?
           (answer && isCorrect) ? (
-            <h3>{`Yes! ${question.correct_answer} is the correct answer! +1 score`}</h3>
+            <>
+              <h3>{`Yes! ${question.correct_answer} is the correct answer! +1 score`}</h3>
+              <GoNext isLastQuestion={isLastQuestion} setIsCorrect={setIsCorrect} setAnswer={setAnswer}/>
+            </>
           ) : (
-            <h3>{`Oh no! ${answer} was not the correct answer. The correct answer was ${question.correct_answer}! -1 score`}</h3>
+            <>
+              <h3>{`Oh no! ${answer} was not the correct answer. The correct answer was ${question.correct_answer}! -1 score`}</h3>
+              <GoNext isLastQuestion={isLastQuestion} setIsCorrect={setIsCorrect} setAnswer={setAnswer}/>
+            </>
           )
           : null
         }
