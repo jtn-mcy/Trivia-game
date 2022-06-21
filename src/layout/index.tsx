3498c28b797a2231ea491bc01e3cb21406fa5e10
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-21 13:07:39 -0700

    add score component

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,20 +2,19 @@ import React from 'react'
 import styles from './index.module.scss'
 import NavBar from '../components/NavBar'
 import CurrentUser from '../components/CurrentUser'
-import { useAppSelector } from '../api'
+import Score from '../components/Score'
+
 type LayoutProps = {
   children?: JSX.Element | JSX.Element[]
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const score = useAppSelector(state => state.score.value);
-  const question = useAppSelector(state => state.question)
+
   return (
     <div className={styles.Layout}>
       <NavBar />
       <CurrentUser />
-      <h2>Score: {score}</h2>
-      <h2>Question {question.index + 1} out of {question.questions.length} </h2>
+      <Score />
       <div className={styles.GameArea}>
       {children}
       </div>
